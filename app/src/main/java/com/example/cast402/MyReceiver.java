541501973cf3ea3f47af9959371096642d9cfc31
zeynab20004 <zkarami316@gmail.com>
2023-11-12 12:47:24 +0330

    first commit

---
+++ app/src/main/java/com/example/cast402/MyReceiver.java
@@ -0,0 +1,36 @@
+package com.example.cast402;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (checkNetwork(context)){
+            Toast.makeText(context, "اتصال برقرار است", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "اتصال برقرار نیست", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private boolean checkNetwork(Context mContext){
+        try {
+            ConnectivityManager conManager =
+                    (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+            @SuppressLint("MissingPermission") NetworkInfo nInfo = conManager.getActiveNetworkInfo();
+            return (nInfo != null && nInfo.isConnected());
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
+
